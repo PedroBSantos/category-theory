3efe1d878f954221ae048b58b500c5c4d55d9086
Pedro Santos <pedrobsantos98@outlook.com>
2024-03-20 23:28:14 -0300

    kleisli category

---
+++ src/category_theory/kleisli.clj
@@ -0,0 +1,44 @@
+(ns category-theory.kleisli 
+  (:require [category-theory.monad :refer [just? new-maybe]]))
+
+; a -> (b, string)
+(defn lower-case [string]
+  [(.toLowerCase string) (str "lower-case " string " ")])
+
+; a -> (b, string)
+(defn upper-case [string]
+  [(.toUpperCase string) (str "upper-case " string " ")])
+
+(defn- pair [a b] [a b])
+
+; Composition
+(defn >=> [f g]
+  (fn [a] 
+    (let [[first-f-result second-f-result] (f a)
+          [first-g-result second-g-result] (g first-f-result)]
+      (pair first-g-result (str second-f-result second-g-result)))))
+
+; Identity
+(defn id [a] (pair a ""))
+
+(defn should-apply? [maybe _] (just? maybe))
+(defmulti >>= should-apply?)
+(defmethod >>= false [maybe _] 
+  (fn [] maybe))
+(defmethod >>= true [maybe f] 
+  (fn [] (f (:value maybe))))
+
+(defn safe-reciprocal [value]
+  (if (not= 0 value)
+    (new-maybe (/ 1 value))
+    (new-maybe)))
+
+(defn safe-root [value]
+  (if (>= value 0)
+    (new-maybe (Math/sqrt value))
+    (new-maybe)))
+
+; a -> M b
+(defn safe-root-reciprocal [value]
+  (let [reciprocal (safe-reciprocal value)]
+    (>>= reciprocal safe-root)))
---
+++ test/category_theory/kleisli_test.clj
@@ -0,0 +1,14 @@
+(ns category-theory.kleisli-test
+  (:require [category-theory.kleisli :refer [safe-root-reciprocal]]
+            [category-theory.monad :refer [just? nothing?]]
+            [clojure.test :refer [deftest is testing]]))
+
+(deftest kleisli-safe-root-reciprocal-test-just
+  (testing "Deveria compor as funções safe-reciprocal e safe-root para um valor x, se x != 0 em safe-reciprocal e x >= 0 em safe-root"
+    (is (just? (let [result (safe-root-reciprocal 5)]
+                 (result))))))
+
+(deftest kleisli-safe-root-reciprocal-test-nothing
+  (testing "Não deveria compor as funções safe-reciprocal e safe-root para um valor x, se x = 0 em safe-reciprocal"
+    (is (nothing? (let [result (safe-root-reciprocal -5)]
+                 (result))))))
